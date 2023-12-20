974a4d08e49b769d55a3ffda51cd7a40457bbbd4
Walker Negrao <1882452@tcs.com>
2023-12-20 11:19:54 -0300

    Aulas 22 e 23 - Implementadas

---
+++ secao3/aula22.py
@@ -0,0 +1,8 @@
+print(int("1"), type(int("1")))
+print(float("12.45"), type(float("12.45")))
+
+print(float(12), type(float(12)))
+print(int(11.223), type(int(11.223)))
+
+print(str(12) + "b", type(str(12)))
+print(str(12.345) + "b", type(str(12.345)))
---
+++ secao3/aula23.py
@@ -0,0 +1,4 @@
+nome_completo = "Walker Esteves Negrão"
+soma_dois_mais_dois = 2 + 2
+
+print(nome_completo, soma_dois_mais_dois)
