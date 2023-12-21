78c3f83d48690e23683fcb45cff5049cec8f11b3
Walker Negrao <1882452@tcs.com>
2023-12-21 16:34:18 -0300

    Aula 34 - Implementada

---
+++ secao3/aula34.py
@@ -0,0 +1,8 @@
+entrada = input("Vc quer 'entrar' ou 'sair'? ")
+
+if entrada == "entrar":
+    print("Vc entrou!!!")
+elif entrada == 'sair':
+    print("Vc saiu!")
+else:
+    print("Vc não digitou nem 'entrar' e nem 'sair'")
