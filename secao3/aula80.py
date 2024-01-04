20266e5484a2d82e503e7b0dd1b32105f7079e51
Walker Negrao <1882452@tcs.com>
2024-01-04 16:41:23 -0300

    Aula 80 - Implementada

---
+++ secao3/aula80.py
@@ -0,0 +1,20 @@
+minha_lista = [1, 2, 3, "quatro", 5.0]
+
+print(minha_lista[0])
+print(minha_lista[3])
+print(minha_lista[-2])
+
+minha_lista[0] = "Walker"
+print(minha_lista)
+
+minha_lista.append("Negrão")
+print(minha_lista)
+
+del minha_lista[4]
+print(minha_lista)
+
+minha_lista.insert(1,133)
+print(minha_lista)
+
+minha_lista.pop(0)
+print(minha_lista)
