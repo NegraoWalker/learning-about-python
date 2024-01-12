85825a243c73609d26c366d7e92ee536cf0a2e37
Walker Negrao <1882452@tcs.com>
2024-01-12 12:00:46 -0300

    Aulas 93 a 95 - Implementados

---
+++ secao3/aula93.py
@@ -0,0 +1,12 @@
+frase = "Olha só que, coisa interessante!!"
+
+lista_palavras = frase.split()
+print(lista_palavras)
+
+lista_palavras = frase.split(",")
+print(lista_palavras)
+
+lista_frase = ['eu','adoro','chocolate']
+
+frase_com_join = '#'.join(lista_frase)
+print(frase_com_join)
